28323fc01edb21640db4fdfdd6cfe3b663c58f11
Enguerrand Decorne <decorne.en@gmail.com>
2021-06-09 17:49:43 +0200

    engil: update remote opam repo for multicore build

--- scripts/version.sh
+++ scripts/version.sh
@@ -3,7 +3,7 @@
 ## `ocaml-version` should be in sync with `README.rst` and
 ## `lib.protocol-compiler/tezos-protocol-compiler.opam`
 
-ocaml_version=4.12.0
+ocaml_version=4.12.0+multicore
 opam_version=2.0
 recommended_rust_version=1.44.0
 
@@ -13,10 +13,9 @@ full_opam_repository_tag=7026cc03199e7315b5ec592610650ff85c21cd90
 
 ## opam_repository is an additional, tezos-specific opam repository.
 ## This value MUST be the same as `build_deps_image_version` in `.gitlab-ci.yml
-opam_repository_tag=327b6d210c4ce8e63c80633df0302c94267f4cff
-opam_repository_url=https://gitlab.com/lthms/tezos-opam-repository-ocaml412
+opam_repository_url=https://github.com/Engil/tezos-opam-repository-412
 opam_repository_git=$opam_repository_url.git
-opam_repository=$opam_repository_git\#$opam_repository_tag
+opam_repository=$opam_repository_git
 
 ## Other variables, used both in Makefile and scripts
 COVERAGE_OUTPUT=_coverage_output
