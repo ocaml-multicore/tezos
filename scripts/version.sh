460f77ee4b8ae15a59d6e9a690f2b806b7c0f159
Sudha Parimala <sudharg247@gmail.com>
2021-07-19 12:31:25 +0530

    Version string is `4.12.0+domains`

--- scripts/version.sh
+++ scripts/version.sh
@@ -3,7 +3,7 @@
 ## `ocaml-version` should be in sync with `README.rst` and
 ## `lib.protocol-compiler/tezos-protocol-compiler.opam`
 
-ocaml_version=4.12.0+multicore
+ocaml_version=4.12.0+domains
 opam_version=2.0
 recommended_rust_version=1.44.0
 
