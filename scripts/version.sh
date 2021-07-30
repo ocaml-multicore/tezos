c9c93b8fbe15a813a81e205bd75a89068fe21115
Enguerrand Decorne <decorne.en@gmail.com>
2021-07-30 10:00:15 +0200

    update URL to ocaml-multicore org rather than Engil

--- scripts/version.sh
+++ scripts/version.sh
@@ -14,12 +14,12 @@ full_opam_repository_tag=7026cc03199e7315b5ec592610650ff85c21cd90
 ## opam_repository is an additional, tezos-specific opam repository.
 ## This value MUST be the same as `build_deps_image_version` in `.gitlab-ci.yml
 opam_repository_tag=1db0e28de40c6416ed75ee092f7a803fe326069d
-opam_repository_url=https://github.com/Engil/tezos-opam-repository
+opam_repository_url=https://github.com/ocaml-multicore/tezos-opam-repository
 opam_repository_git=$opam_repository_url.git
 opam_repository=$opam_repository_git
 
 ## for sapling param, fork is on github, url convention not the same in install_sapling_parameters
-raw_opam_repository_url=https://raw.githubusercontent.com/Engil/tezos-opam-repository/4.12.0%2Bdomains
+raw_opam_repository_url=https://raw.githubusercontent.com/ocaml-multicore/tezos-opam-repository/4.12.0%2Bdomains
 
 ## Other variables, used both in Makefile and scripts
 COVERAGE_OUTPUT=_coverage_output
