e91082f3c90b96a59beac7fe7ceae1e140693e0e
Sudha Parimala <sudharg247@gmail.com>
2022-02-09 16:02:16 +0530

    trunk changes

--- scripts/version.sh
+++ scripts/version.sh
@@ -13,7 +13,7 @@
 ## This script is also sourced in the Makefile, as such it should be compatible
 ## with both the make and sh syntax
 
-export ocaml_version=4.12.0+domains
+export ocaml_version=5.00.0+dev0-2021-11-05
 export opam_version=2
 export recommended_rust_version=1.52.1
 export recommended_node_version=14.12.0
@@ -27,7 +27,7 @@ export full_opam_repository_tag=98a71e3eebaa50faf3c154f2f766a52564654b9d
 export opam_repository_url=https://github.com/ocaml-multicore/tezos-opam-repository
 export opam_repository_tag=a1545517aa61be25303cd4e8731dc22f000887f8
 export opam_repository_git=$opam_repository_url.git
-export opam_repository=$opam_repository_git\#update-24-01-take-2
+export opam_repository=$opam_repository_git\#trunk
 
 ## Other variables, used both in Makefile and scripts
 export COVERAGE_OUTPUT=_coverage_output
